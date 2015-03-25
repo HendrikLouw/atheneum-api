33a27c9c17d801a3515106bc69e045d1892c30e0
Hendrik Louw <hlouw@thoughtworks.com>
2015-03-25 08:20:53 +0200

    fresh start on the library

--- atheneum.rb
+++ atheneum.rb
@@ -3,7 +3,5 @@ require 'json'
 
 require_relative './domain/book'
 require_relative './domain/book_isbn'
-require_relative './domain/library'
 
 require_relative './api/book_api'
-require_relative './api/library_api'
